9ce1f051a9a246aea5cd031d60bf1b2f9cb1afc6
Daniel Kuznetsov <danielkuznetsov@Daniels-MBP.lan>
2022-09-26 22:41:19 -0700

    example of receiving data from a user

---
+++ chapter-2/example-2-17/example-2-17.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int feet, inches;
+
+    cout << "Enter two integers by one or more spaces: ";
+    cin >> feet >> inches;
+    cout << endl;
+
+    cout << "Feet = " << feet << endl;
+    cout << "Inches = " << inches << endl;
+
+    return 0;
+}
